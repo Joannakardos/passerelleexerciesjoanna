dfe838b0e07a3637fb01a136a2b8f4c9f12d8b9a
Joanna <kardos.joanna1@gmail.com>
2025-09-08 09:32:31 +0200

    ha

---
+++ JavaScript/js/javaTypes.js
@@ -0,0 +1,7 @@
+let num = 34;
+console.log(num)/*its print out basic*/;
+console.info(typeof num);
+
+num = false;
+console.log(num)/*its print out basic*/;
+console.info(typeof num);/*it tells you whats the type of it*/
