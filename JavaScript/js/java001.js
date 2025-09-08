4ba5a795631722f6719696f3b8efe1759df6c611
Joanna <kardos.joanna1@gmail.com>
2025-09-08 09:18:33 +0200

    eb

--- JavaScript/js/java001.js
+++ JavaScript/js/java001.js
@@ -2,13 +2,13 @@ let huh = "huh let";
 var ah = "ah var"
 try {
     if (true) {
-        let huhscoped = "lol"/* c'est que dans le block la"*/
+        let huhscoped = "lol"/* c'est QUE dans le block la"*/
         var ahglobale = " oui"
         console.log(huhscoped, ahglobale);
     }
 }
 catch (error) {
     console.info("I caught the error");
-    console.log(huhscoped);
+    /* console.log(huhscoped);* ce n'est definis que dans le bloc precedent donc ic ica ne s'affichera pas*/
     console.log(ahglobale);
 }
